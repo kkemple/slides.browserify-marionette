cb4b43b8f46dcc7bd71a083c0a926e3c64fbf6d5
Kurtis Kemple <kurtiskemple@gmail.com>
2015-01-01 01:30:16 -0500

    added slides for marionette app configuration and running

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 
 var Marionette = require('./libs/marionette'),
     Backbone = require('./libs/backbone'),
-    _ = require('./libs/underscore'),
     vent = require('./config/events'),
     app;
 
@@ -39,7 +38,7 @@ app.on('start', function() {
 
 module.exports = app;
 
-},{"./config/events":3,"./libs/backbone":8,"./libs/marionette":10,"./libs/underscore":11}],2:[function(require,module,exports){
+},{"./config/events":3,"./libs/backbone":8,"./libs/marionette":10}],2:[function(require,module,exports){
 'use strict';
 
 var Slide = require('../models/slide'),
@@ -185,6 +184,8 @@ module.exports = [
     { id: 'gettingSetUpShimmingGlobals', title: 'Browserify + Marionette - Shimming Globals' },
     { id: 'gettingSetUpUnderstandingTransforms', title: 'Browserify + Marionette - Understanding Transforms' },
     { id: 'marionette', title: 'Browserify + Marionette - Adding Marionette To the Mix', titleSlide: true },
+    { id: 'marionetteAppConfig', title: 'Browserify + Marionette - Configuring Your Marionette App' },
+    { id: 'marionetteAppStart', title: 'Browserify + Marionette - Starting Your Marionette App' },
 ];
 },{}],6:[function(require,module,exports){
 module.exports.code = require('../../../partials/slides/code.hbs');
@@ -197,12 +198,14 @@ module.exports.gettingSetUpPackageJson = require('../../../partials/slides/getti
 module.exports.gettingSetUpShimmingGlobals = require('../../../partials/slides/gettingSetUpShimmingGlobals.hbs');
 module.exports.gettingSetUpUnderstandingTransforms = require('../../../partials/slides/gettingSetUpUnderstandingTransforms.hbs');
 module.exports.marionette = require('../../../partials/slides/marionette.hbs');
+module.exports.marionetteAppConfig = require('../../../partials/slides/marionetteAppConfig.hbs');
+module.exports.marionetteAppStart = require('../../../partials/slides/marionetteAppStart.hbs');
 module.exports.whyUse = require('../../../partials/slides/whyUse.hbs');
 module.exports.whyUseBuildIntegration = require('../../../partials/slides/whyUseBuildIntegration.hbs');
 module.exports.whyUseCommonJS = require('../../../partials/slides/whyUseCommonJS.hbs');
 module.exports.whyUseNpmModules = require('../../../partials/slides/whyUseNpmModules.hbs');
 
-},{"../../../partials/slides/code.hbs":142,"../../../partials/slides/entry.hbs":143,"../../../partials/slides/gettingSetUp.hbs":144,"../../../partials/slides/gettingSetUpConsole.hbs":145,"../../../partials/slides/gettingSetUpGrunt.hbs":146,"../../../partials/slides/gettingSetUpNonNpmDeps.hbs":147,"../../../partials/slides/gettingSetUpPackageJson.hbs":148,"../../../partials/slides/gettingSetUpShimmingGlobals.hbs":149,"../../../partials/slides/gettingSetUpUnderstandingTransforms.hbs":150,"../../../partials/slides/marionette.hbs":151,"../../../partials/slides/whyUse.hbs":152,"../../../partials/slides/whyUseBuildIntegration.hbs":153,"../../../partials/slides/whyUseCommonJS.hbs":154,"../../../partials/slides/whyUseNpmModules.hbs":155}],7:[function(require,module,exports){
+},{"../../../partials/slides/code.hbs":142,"../../../partials/slides/entry.hbs":143,"../../../partials/slides/gettingSetUp.hbs":144,"../../../partials/slides/gettingSetUpConsole.hbs":145,"../../../partials/slides/gettingSetUpGrunt.hbs":146,"../../../partials/slides/gettingSetUpNonNpmDeps.hbs":147,"../../../partials/slides/gettingSetUpPackageJson.hbs":148,"../../../partials/slides/gettingSetUpShimmingGlobals.hbs":149,"../../../partials/slides/gettingSetUpUnderstandingTransforms.hbs":150,"../../../partials/slides/marionette.hbs":151,"../../../partials/slides/marionetteAppConfig.hbs":152,"../../../partials/slides/marionetteAppStart.hbs":153,"../../../partials/slides/whyUse.hbs":154,"../../../partials/slides/whyUseBuildIntegration.hbs":155,"../../../partials/slides/whyUseCommonJS.hbs":156,"../../../partials/slides/whyUseNpmModules.hbs":157}],7:[function(require,module,exports){
 'use strict';
 
 var Marionette = require('../libs/marionette'),
@@ -10985,24 +10988,38 @@ module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"
 // hbsfy compiled Handlebars template
 var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<h1>So why use Browserify?</h1>";
+  return "<div class=\"code-snippet\">\n    <pre>\n        <code class=\"hljs javascript\">\n/* js/app.js */\n\nvar Marionette = require('./libs/marionette'),\n    Backbone = require('./libs/backbone'),\n    vent = require('./config/events'),\n    app;\n\napp = new Marionette.Application({\n    regions: {\n        mainRegion: '#app'\n    }\n});\napp.router = new Backbone.Router();\nvent.on('app:navigate', function(path, trigger) { ... });\napp.on('start', function() { Backbone.history.start(); });\n\nmodule.exports = app;\n        </code>\n    </pre>\n</div>";
   },"useData":true});
 
 },{"hbsfy/runtime":25}],153:[function(require,module,exports){
 // hbsfy compiled Handlebars template
 var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<h1>Easy Build Integration</h1>\n<img src=\"img/gulp-grunt.png\" alt=\"\">";
+  return "<div class=\"code-snippet\">\n    <pre>\n        <code class=\"hljs javascript\">\n\n/* js/main.js */\n\nvar app = require('./app'),\n    Slides = require('./collections/slides'),\n    slidesConfig = require('./config/slides'),\n    Presentation = require('./views/presentation'),\n    reqres = require('./config/reqres'),\n    slides,\n    presentation;\n\nslides = new Slides(slidesConfig);\npresentation = new Presentation({ collection: slides });\nreqres.setHandler('slides', function() { return slides; });\napp.addInitializer(function() { app.mainRegion.show(presentation); });\n\n// Finally, start the app\napp.start();\n\n        </code>\n    </pre>\n</div>\n";
   },"useData":true});
 
 },{"hbsfy/runtime":25}],154:[function(require,module,exports){
 // hbsfy compiled Handlebars template
 var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<h1>CommonJS Style Modules</h1>\n<div class=\"code-snippet\">\n    <pre>\n        <code class=\"hljs javascript\">\nvar Marionette = require('./libs/marionette'),\n    app;\n\napp = new Marionette.Application({\n    regions: {\n        mainRegion: '#app'\n    }\n});\n\n// handle any app configuration\n\nmodule.exports = app;\n        </code>\n    </pre>\n</div>";
+  return "<h1>So why use Browserify?</h1>";
   },"useData":true});
 
 },{"hbsfy/runtime":25}],155:[function(require,module,exports){
+// hbsfy compiled Handlebars template
+var HandlebarsCompiler = require('hbsfy/runtime');
+module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<h1>Easy Build Integration</h1>\n<img src=\"img/gulp-grunt.png\" alt=\"\">";
+  },"useData":true});
+
+},{"hbsfy/runtime":25}],156:[function(require,module,exports){
+// hbsfy compiled Handlebars template
+var HandlebarsCompiler = require('hbsfy/runtime');
+module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<h1>CommonJS Style Modules</h1>\n<div class=\"code-snippet\">\n    <pre>\n        <code class=\"hljs javascript\">\nvar Marionette = require('./libs/marionette'),\n    app;\n\napp = new Marionette.Application({\n    regions: {\n        mainRegion: '#app'\n    }\n});\n\n// handle any app configuration\n\nmodule.exports = app;\n        </code>\n    </pre>\n</div>";
+  },"useData":true});
+
+},{"hbsfy/runtime":25}],157:[function(require,module,exports){
 // hbsfy compiled Handlebars template
 var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
